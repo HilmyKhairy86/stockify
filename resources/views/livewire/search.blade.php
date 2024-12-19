55b1755a890bf661f5f7baa950b44d44956d99c2
HilmyKhairy86 <HilmyKhairy86@gmail.com>
2024-12-19 09:24:34 +0700

    .

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -163,7 +163,7 @@
             <div x-data="{ importdatamodal: false }" x-cloak="{display: none}" x-init="open = false" @keydown.escape.window="open = false" x-bind:class="{ 'overflow-hidden': open }"  class="relative">
                 <!-- Button to toggle the modal -->
                 <button id="importmodal" @click="importdatamodal = true" class="flex items-center justify-center dark:bg-blue-600 bg-blue-600 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800" type="button">
-                    <i class="fa-solid fa-file-import"></i>
+                    <i class="fa-solid fa-file-import mr-2"></i>
                 Import
                 </button>
 
